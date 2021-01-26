3c5144f38340a6f7cebfc47b6e7e3051072ef25c
Tim Badgery-Parker <tim.badgery-parker@mq.edu.au>
2021-01-26 20:06:51 +1100

    Add some tests

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://timbp.github.io/timbpMisc.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://timbp.github.io/timbpMisc.jl/dev)
+
+Some types and functions I find useful in my own work.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
-using timbpMisc
-using Test
+using timbpMisc, Test, Dates, Distributions
 
 @testset "timbpMisc.jl" begin
-    # Write your tests here.
+  @testset "Financial years" begin
+    d = Dates.Date("2019")
+    @test FinancialYear(2019).value == 2019
+    @test FinancialYear(d).value == 2019
+    @test FinancialYear("2019").value == 2019
+    @test FinancialYear("2019-06").value == 2019
+    @test FinancialYear("2019-07").value == 2020
+    @test FinancialYear("2019-06-30").value == 2019
+    @test FinancialYear("2019-07-01").value == 2020
+    @test FinancialYear(2019) - FinancialYear(2017) == Year(2)
+    @test FinancialYear(2017) + Year(2) == FinancialYear(2019)
+    @test FinancialYear(2017) - Year(2) == FinancialYear(2015)
+    @test show(FinancialYear(d)) == print("2018/19")
+    @test print(FinancialYear(d)) == 2019
+    @test (FinancialYear(2019) < FinancialYear(2020)) == true
+  end
+  @testset "Control limits" begin
+    @test controllimits(0.025, 50, .3) == 0.16613938031193454
+    @test controllimits(0.975, 50, .3) == 0.4201358867168803
+    @test controllimits.((0.025, 0.975), 50, .3) == (0.16613938031193454, 0.4201358867168803)
+  end
 end
